2e12d8b5037f5d6dacccccbbd519d74e4ae81c7f
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2014-07-23 13:39:22 +0200

    added highlighting of second char

--- js/classes/Cell.js
+++ js/classes/Cell.js
@@ -15,8 +15,29 @@ var xsolve;
         this.$elem.addClass('highlight');
     };
 
-    Cell.prototype.neighbors = function(input) {
-        console.log('neighbors ' + this.neighbor);
+    Cell.prototype.path = function(input, direction, rows) {
+        // TODO direction can be multiple directions
+        // if direction === any, it is the second character
+        //console.log('neighbors ' + this.neighbor);
+        if( direction === 'any' && input.length > 0) {
+            var nextChar = input[0];
+            //console.log('nextChar = ' + nextChar);
+
+            // iterate over all neighbors
+            for( var name in this.neighbor ) {
+                //console.log(name);
+                var x = this.neighbor[name][0];
+                var y = this.neighbor[name][1];
+                // console.log('x=' + x );
+                // console.log('neighbor ' + rows[x][y].char);
+                if(rows[x][y].char === nextChar) {
+                    // console.log('found ' + nextChar + ' in neighbor');
+                    rows[x][y].highlight();
+                    // TODO find the next char, but only in the same direction
+                    // e.g. cell.path(inputtail, samedirection, rows);
+                }
+            }
+        }
         //this.$elem.addClass('highlight');
     };
 
